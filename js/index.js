b3180f9f3da26ec69b23a4c64f7f8d1244ab4d11
brantchyoga <brantrporter@gmail.com>
2018-04-12 13:20:34 -0700

    lots o stuff

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-$( document ).ready(function() {
+$(document).ready(()=>{
+  var about = $('#about');
+  about.hide();
+  var aboutCount = 0;
+  $('#showAbout').click(()=>{
+    console.log(aboutCount);
+    if (aboutCount%2 === 0 || aboutCount===0){
+      about.show()
+      aboutCount++;
+    } else {
+      about.hide();
+      aboutCount++;
+    }
 
-  $(".button-collapse").sideNav();
-  $('.carousel').carousel({});
-});
+  })
+})
